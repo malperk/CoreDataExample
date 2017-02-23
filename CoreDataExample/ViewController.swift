cea7753a959d316d5f7e45588544cc9f9aef0f60
Alper KARATAŞ <AlperKaratas@kkb.com.tr>
2017-02-23 10:30:13 +0300

    Empty project

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Alper KARATAŞ on 23/02/2017.
+//  Copyright © 2017 Alper KARATAŞ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
