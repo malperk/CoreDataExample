e6f69f3705df79cb5e711b061c808b9b2d9b4b6e
Alper KARATAŞ <AlperKaratas@kkb.com.tr>
2017-02-23 11:31:42 +0300

    UI design

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tvMain: UITableView!
+    var items = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tvMain.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - AddItem
+    @IBAction func addItem(_: Any) {
+        let alert = UIAlertController(title: "New Item",
+                                      message: "Add a new item",
+                                      preferredStyle: .alert)
+
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default,
+                                       handler: { (_: UIAlertAction) -> Void in
+
+                                           let textField = alert.textFields!.first
+                                           self.items.append(textField!.text!)
+                                           self.tvMain.reloadData()
+        })
+
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default) { (_: UIAlertAction) -> Void in
+        }
+
+        alert.addTextField {
+            (_: UITextField) -> Void in
+        }
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true, completion: nil)
     }
 
+    // MARK: - UITableViewDataSource
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell!.textLabel!.text = items[indexPath.row]
+        return cell!
+    }
 
 }
-
